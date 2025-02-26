26e4cbd7112da2113e42d4942f5df7332bcea9cd
Khai <jonathankhai.tran@gmail.com>
2025-02-26 06:55:28 -0600

    analyze/steer

--- components/gemmascope.test.js
+++ components/gemmascope.test.js
@@ -116,10 +116,89 @@ test('analyze features page', async ({ page }) => {
     await page.locator('textarea[name="The features in Gemma Scope have labels"]').isVisible();
 });
 
+test('olympic sports button', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#analyze');
+    await page.getByText('Olympic sports').click();
+    await expect(page.getByText('What do these activations (in green) have in common?')).toBeVisible();
+});
+
+test('famous cities button', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#analyze');
+    await page.getByText('famous cities').click();
+    await expect(page.getByText('What do these activations (in green) have in common?')).toBeVisible();
+});
+
+test('reference to animals button', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#analyze');
+    await page.getByText('references to animals').click();
+    await expect(page.getByText('Nice! This was tricky, because there are two possibly correct answers.')).toBeVisible();
+});
+
+test('none of these button', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#analyze');
+    await page.getByText('none of these').click();
+    await expect(page.getByText('Nice! This was tricky, because there are two possibly correct answers.')).toBeVisible();
+});
+
+test('analyze steer', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#analyze');
+    
+    //unlocked after doing the demo, or pressing skip demo
+    await page.getByText('skip example').first().click();
+    const analyzeSteer = page.getByPlaceholder('Can you write a text snippet that activates it?');
+    await analyzeSteer.fill('test');
+    await analyzeSteer.press('Enter');
+    
+    await expect(page.getByText('0.00')).toBeVisible(); 
+});
+
+test('puzzles labels', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#analyze');
+
+    //unlocked after doing analyze steer, or pressing skip analyze
+    await page.getByText('skip analyze').first().click();
+
+    //test all 3 buttons
+    const labelButtons = await page.getByRole('button', { name: 'Reveal Our Label'}).all();
+    //click each button
+    for ( let i = 0; i < labelButtons.length; i++) {
+        await labelButtons[i].click();
+    }
+
+    //check labels
+    await expect(page.getByText('Lies and falsehoods')).toBeVisible();
+    await expect(page.getByText('Misspellings or typos')).toBeVisible();
+    await expect(page.getByText('Bad/cringe stories')).toBeVisible();
+});
+
+test('next steer gemma button', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#analyze');
+
+    //unlocked after puzzles, or pressing skip analyze
+    await page.getByText('skip analyze').nth(1).click();
+    await page.getByText(' Next - Steer Gemma').click();
+    await expect(page).toHaveURL('https://www.neuronpedia.org/gemma-scope#steer');
+});
+
+
 test('steer page', async ({ page }) => {
     await page.goto('https://neuronpedia.org/gemma-scope#steer');
 
     await page.locator('textarea[name="Let\'s put these features to use"]').isVisible();
+});
+
+test('steer demo', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#steer');
+
+    //check for demo question
+    await expect(page.getByText('What are you?').first()).toBeVisible();
+});
+
+test('next do more button', async ({ page }) => {
+    await page.goto('https://neuronpedia.org/gemma-scope#steer');
+
+    await page.getByText('Next - Do More').click();
+    await expect(page).toHaveURL('https://www.neuronpedia.org/gemma-scope#learn');
 });
 
 test('do more page', async ({ page }) => {
